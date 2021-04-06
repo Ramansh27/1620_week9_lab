0ec869122d52fecdad2311277ac68b859e3df73d
Ramansh Chhabra <rchhabra6@my.bcit.ca>
2021-04-06 04:14:50 -0700

    modified .js file with required functions

---
+++ app.js
@@ -0,0 +1,14 @@
+let num1 = 27
+let num2 = 12
+const name = "Ramansh"
+const boolc = true
+
+if (num1 === num2)
+{
+    console.log("They match")   }
+else {
+    console.log("They don't match")     }
+
+for (letter of name){
+    console.log(letter)
+}
